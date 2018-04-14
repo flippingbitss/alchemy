33f6cd138fba73febbfc846bbcdbf23260142240
snorock <ibrovche@gmail.com>
2018-04-14 11:49:02 -0400

    Added health potion, player attack mechanicks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,23 +9,28 @@ public class PlayerController : MonoBehaviour {
     public float MaxHealth = 100;
     public float AttackSpeed = 5; //attacks per second
     public float Deffense = 0; //percent
+    public float Health;
 
     //Privates
-    private float Health;
-    private GameObject Enemy = null;
+    private EnemyController enemyController = null;
     private GameController gameController = null;
 
 
 	// Use this for initialization
 	void Start () {
         Health = MaxHealth;
-        Enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemyController = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyController>();
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+
+        InvokeRepeating("AttackEnemy", 1f, 1 / this.AttackSpeed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!gameController.isBattling)
+        {
+            CancelInvoke();
+        }
 	}
 
     public void TakeDamage(float damage)
@@ -38,4 +43,10 @@ public class PlayerController : MonoBehaviour {
             gameController.isBattling = false;
         }
     }
+    public void AttackEnemy()
+    {
+        enemyController.TakeDamage(this.Damage);
+        Debug.Log("player attacks" + this.Damage);
+    }
+
 }
