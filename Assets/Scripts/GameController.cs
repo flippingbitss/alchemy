06b1b931bfad504ddac0ffaa947e0994122eda13
snorock <ibrovche@gmail.com>
2018-04-14 11:13:48 -0400

    added isBattling parameter for game controller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    //Publics
+    public bool isBattling; //is the battle between player and enemy going at the moment or paused/over
+
 	// Use this for initialization
 	void Start () {
+        isBattling = true;
 		
 	}
 	
