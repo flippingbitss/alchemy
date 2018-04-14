052dcf4518d63a1ccd0ca19f95ab4463f4498c7c
q4-charles <charlest@q4inc.com>
2018-04-14 12:20:25 -0400

    add level up

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,35 +10,44 @@ public class EnemyController : MonoBehaviour {
     public float MaxHealth = 100;
     public float AttackSpeed = 5; //attacks per second
     public float Deffense = 0; //percent
+    public bool isDead = false;
+    public int level = 1;
 
     private float Health;
     private float Speed;
+    private float Level;
+    private float Def;
 
     // Use this for initialization
     void Start()
     {
-        Health = MaxHealth;
-        Speed = AttackSpeed;
+        Health = MaxHealth * level;
+        Speed = AttackSpeed * level;
+        Def += Deffense * level;
+
+        Level = level;
         InvokeRepeating("DoDamage", 0, Speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!controller.isBattling)
+        if(!controller.isBattling || isDead)
         {
             CancelInvoke();
         } else
         {
             InvokeRepeating("DoDamage", 0, Speed);
+            InvokeRepeating("HealOverTime", 60, Speed * 10);
         }
 
-        InvokeRepeating("HealOverTime", 60, Speed * 10);
+        
     }
 
     public void HealOverTime()
     {
         this.Health += 30;
+        Debug.Log("Enemy healed for 30 points");
     }
 
     public void TakeDamage(float damage)
@@ -46,15 +55,22 @@ public class EnemyController : MonoBehaviour {
         this.Health -= damage * (1 - (Deffense / 100));
         if (this.Health <= 0)
         {
+            CancelInvoke();
             this.Health = 0;
             Debug.Log("Enemy is dead");
             controller.isBattling = false;
-            CancelInvoke();
+            isDead = true;
         }
     }
 
     public void DoDamage()
     {
         player.TakeDamage(this.Damage);
+    }
+
+    public void LevelUpEnemy()
+    {
+        isDead = false;
+        level += 1;
     }
 }
