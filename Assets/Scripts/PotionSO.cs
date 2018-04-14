b984a1754ff911c22e1dc4a4c7236affe9d0a2a2
Eddie Song <eddie.y.song@gmail.com>
2018-04-14 11:56:33 -0400

    added potionSO

---
+++ Assets/Scripts/PotionSO.cs
@@ -0,0 +1,12 @@
+﻿// Eddie Song 2017-07-04
+// Scriptable object to contain potion data
+
+using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Potion", order = 1)]
+public class PotionSO : ScriptableObject
+{
+
+    public float HealingAmount;
+}
