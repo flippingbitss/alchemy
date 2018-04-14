1d24f864f097d9360a9965b28d26611a6143aeef
Manpreet Singh Matharu <matharumanpreet00@gmail.com>
2018-04-14 11:15:13 -0400

    added BuffsController class and IPotion interface

---
+++ Assets/Scripts/BuffsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class BuffsController : MonoBehaviour
+{
+
+    public List<IPotion> potions;
+    public IPotion activePotion;
+
+    PlayerController playerController;
+
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (activePotion != null)
+            {
+                activePotion.Consume(playerController);
+            }
+        }
+
+    }
+
+    public void AddPotion(IPotion potion){
+        Debug.Log("added potion " + potion.Name);
+        potions.Add(potion);
+    }
+
+    public void SelectPotion(int index){
+        if (index < 0 || index >= potions.Count) return;
+        activePotion = potions[index];
+    }
+}
